db058d5333bced23abbb8dd0a012c2c7f09132d8
jeongh <jeongh389@gmail.com>
2018-12-09 23:29:38 +0900

    table

---
+++ datatables/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import DataTablesPage from "./DataTablesPage";
+
+class App extends Component {
+  render() {
+    return (
+     <DataTablesPage/>
+    );
+  }
+}
+
+export default App;
